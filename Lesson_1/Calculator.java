8db9b79023719877b73c2480dc217d8f982e1d70
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-05-17 17:07:04 +0500

    implement 1/2 final HW

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+
+   public static void main(String[] args) {
+   //Калькулятор;
+   System.out.println("\nКалькулятор");
+   double num1 = 100;
+   double num2 = 8;
+   char sign = '/';
+   double result = 0;
+   if ((num1 > 0 & num1 % 1 == 0) & (num2 > 0 & num2 % 1 == 0)) {
+      if (sign == '+') {
+         result = num1 + num2;
+      } else if (sign == '-') {
+         result = num1 - num2;
+      } else if (sign == '*') {
+         result = num1 * num2;
+      } else if (sign == '/') {
+         result = num1 / num2;
+      } else if (sign == '^') {
+         result = 1;
+         for (double i = 1; i <= num2; i++) {
+             result *= num1;
+         }
+      } else if (sign == '%') {
+         result = (num1 / 100) * num2;
+      }
+      System.out.printf("\nЧисло %.0f %c число %.0f = %.2f", num1, sign, num2, result);
+   } else {
+      System.out.println("\nЧисла отрицательные или не целые, для расчета введите целые положительные числа");
+   }
+}
