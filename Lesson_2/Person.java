0ac08d9ffd95d53a0e1939b3f779a6de9537ffbf
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-05-22 17:05:17 +0500

    implement 1hw

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+	String sex = "Male";
+	String name = "Evgeniy";
+	int hight = 184;
+	float weight = 85.6f;
+	int age = 33;
+
+	boolean learnJava() {
+		return true;
+	}
+	void move() {
+		System.out.println("Я иду");	
+	}
+
+	void sleep() {
+		System.out.println("Я уснул");
+	}
+
+	void sit () {
+		System.out.println("Сел");
+		
+	}
+	void speak() {
+		System.out.println("Не спать! Учиться, учиться и учиться");
+	}
+
+}
