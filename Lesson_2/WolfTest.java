5b91d187f33ffb9d150e1170555b1a10fdc6f1b8
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-05-26 14:18:18 +0500

    implement hw_2

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,7 +1,22 @@
 public class WolfTest {
 
 	public static void main(String[] args) {
+		System.out.println("WolfTest\n");
 		Wolf wolfPet = new Wolf();
-		System.out.println(wolfPet.age);
+		wolfPet.sex = "male";
+		System.out.println("Пол волка = " + wolfPet.sex);
+		wolfPet.name = "archie";
+		System.out.println("Кличка волка = " + wolfPet.name);
+		wolfPet.color = "black";
+		System.out.println("Цвет волка = " + wolfPet.color);
+		wolfPet.weight = 56;
+		System.out.println("Вес волка = " + wolfPet.weight + " кг");
+		wolfPet.age = 8;
+		System.out.println("Возраст волка = " + wolfPet.age + "лет ");
+		wolfPet.go(); 
+		wolfPet.sit();
+		wolfPet.run();
+		wolfPet.howl();
+		wolfPet.hunt();
 	}	
 }
