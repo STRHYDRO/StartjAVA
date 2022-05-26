c80a9bd150bddc38d8e75a6bcad79cb44dbf24f8
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-05-26 15:03:44 +0500

    implement 2HW_2

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,27 +1,26 @@
 public class Wolf {
-	String sex = " ";
-	String name = " "; 
-	String color = " ";
-	float weight = 0;
-	int age = 5;
-    
-    void go() {
-    	System.out.println("Идти");
-    }
+	String sex;
+	String name; 
+	String color;
+	float weight;
+	int age;
+	
+	void go() {
+		System.out.println("Идти");
+	}
 
-    void sit() {
-    	System.out.println("Сидеть");
-    }
-
-    void run() {
-    	System.out.println("Бежать");
-    }
+	void sit() {
+		System.out.println("Сидеть");
+	}
+	void run() {
+		System.out.println("Бежать");
+	}
 
 	void howl() {
-    	System.out.println("Выть");
-	}    	
-	    	
+		System.out.println("Выть");
+	}		
+			
 	void hunt() {
-    	System.out.println("Охотиться");
-    }
+		System.out.println("Охотиться");
+	}
 }	    
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -3,16 +3,11 @@ public class WolfTest {
 	public static void main(String[] args) {
 		System.out.println("WolfTest\n");
 		Wolf wolfPet = new Wolf();
-		wolfPet.sex = "male";
-		System.out.println("Пол волка = " + wolfPet.sex);
-		wolfPet.name = "archie";
-		System.out.println("Кличка волка = " + wolfPet.name);
-		wolfPet.color = "black";
-		System.out.println("Цвет волка = " + wolfPet.color);
-		wolfPet.weight = 56;
-		System.out.println("Вес волка = " + wolfPet.weight + " кг");
-		wolfPet.age = 8;
-		System.out.println("Возраст волка = " + wolfPet.age + "лет ");
+		System.out.println("Пол волка = " + (wolfPet.sex = "male"));
+		System.out.println("Кличка волка = " + (wolfPet.name = "archie"));
+		System.out.println("Цвет волка = " + (wolfPet.color = "black"));;
+		System.out.println("Вес волка = " + (wolfPet.weight = 56) + " кг");
+		System.out.println("Возраст волка = " + (wolfPet.age = 8) + "лет ");
 		wolfPet.go(); 
 		wolfPet.sit();
 		wolfPet.run();
