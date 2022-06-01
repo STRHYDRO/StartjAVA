c98e092ef1f1f97bf0c8bb959bb2abb22a56036a
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-06-01 18:50:59 +0500

    only me work-home

--- Lesson_2/Test.java
+++ Lesson_2/Test.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
-public class Test{
+public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
         Scanner scanner = new Scanner(System.in);
-        int choice = 0;
-        int no = 1;
-        int yes = 5;
+        String choice = "";
         do {
-        System.out.println("\nХотите продолжить карусельку? [yes = 5/no =1]:");
-        choice = scanner.nextInt();
+        System.out.println("Это самый внешний цикл");
         do {
-            if (choice != yes) {
-                System.out.println("\nХотите продолжить карусельку? [yes = 5/no =1]:");
-                choice = scanner.nextInt();   
-            } else {
-            }
-            } while (choice == no);
-        } while (choice == no);
+        System.out.println("\nХотите продолжить карусельку? [yes/no]:");
+        System.out.println("Это основной цикл");
+        choice = scanner.nextLine();
+        while (!(choice.equals("yes"))) {
+        //     if (choice.equals("yes")) {  
+        //         break;  
+        //     } else {
+        // }       
+        }
+        } while (!((choice.equals("no")) || (choice.equals("yes"))));
+        } while (!(choice.equals("no")));
     }
 }
