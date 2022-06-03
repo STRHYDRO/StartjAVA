e5de0da840620b461eb1524492a7cebd1b868b2d
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-06-03 21:53:39 +0500

    double choice final HW_2

--- Lesson_2/CalcTest.java
+++ Lesson_2/CalcTest.java
@@ -6,6 +6,7 @@ public class CalcTest {
         Calculator calcTest = new Calculator();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Calculator test\n");
+        String choice;
         do {
             System.out.println("Введите первое число");
             calcTest.setNum1(scanner.nextInt());
@@ -15,17 +16,13 @@ public class CalcTest {
             calcTest.setNum2(scanner.nextInt());
             calcTest.calculate();
             calcTest.printOut();
-            System.out.println("\nХотите продолжить вычисления ? [yes/no]:");
-            calcTest.choice = scanner.nextLine();
-            calcTest.choice = scanner.nextLine();
-            while (!(calcTest.choice.equals(calcTest.no))) {
-                if (!(calcTest.choice.equals(calcTest.yes))) {
-                    System.out.println("\nХотите продолжить вычисления ? [yes/no]:");
-                    calcTest.choice = scanner.nextLine();   
-                } else { 
-                    break;  
-                }
-            }
-        } while (!(calcTest.choice.equals(calcTest.no)));
-    }
+            do {
+                System.out.println("\nХотите продолжить вычисления ? [yes/no]:");
+                choice = scanner.nextLine();
+                choice = scanner.nextLine();
+            } while (!((choice.equals("no")) || (choice.equals("yes"))));
+        } while (!(choice.equals("no")));
+    } 
 }
+
+
