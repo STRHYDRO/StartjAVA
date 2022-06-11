96c233ccd9486e30848c9cfa4d104dc9b527c43b
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-06-11 20:38:59 +0500

    3\4 implement final HW_2.2

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -1,13 +1,16 @@
 public class Jaeger {
 
+	// private Jaeger() {
 	private String modelName;
-	private String mark;
-	private String origin;
-	private float height;
-	private float weight;
+	private int mark;
+	private double height;
+	private double weight;
 	private int speed;
 	private int strength;
 	private int armor;
+	private int murder;
+	private String status;
+	private String lauching;
 
 	public String getModelName() {
 		return modelName;
@@ -17,35 +20,27 @@ public class Jaeger {
 		this.modelName = modelName;
 	}
 
-	public String getMark() {
+	public int getMark() {
 		return mark;
 	}
 
-	public void setMark(String mark) {
+	public void setMark(int mark) {
 		this.mark = mark;
 	}
 
-	public String getOrigin() {
-		return origin;
-	}
-
-	public void setOrigin(String origin) {
-		this.origin = origin;
-	}
-
-	public float getHeight() {
+	public double getHeight() {
 		return height;
 	}
 
-	public void setHeight(float height) {
+	public void setHeight(double height) {
 		this.height = height;
 	}
 
-	public float getWeight() {
+	public double getWeight() {
 		return weight;
 	}
 
-	public void setWeight(float weight) {
+	public void setWeight(double weight) {
 		this.weight = weight;
 	}
 
@@ -57,19 +52,82 @@ public class Jaeger {
 		this.speed = speed;
 	}
 
-	public int getStreight() {
-		return streight;
+	public int getStrength() {
+		return strength;
 	}
 
-	public void setStreight(float streight) {
-		this.streight = streight;
+	public void setStrength(int strength) {
+		this.strength = strength;
 	}
 
 	public int getArmor() {
 		return armor;
 	}
 
-	public void setArmor(float armor) {
+	public void setArmor(int armor) {
 		this.armor = armor;
 	}
+
+	public int getMurder() {
+		return murder;
+	}
+
+	public void setMurder(int murder) {
+		this.murder = murder;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getLauching() {
+		return lauching;
+	}
+
+	public void setLauching(String lauching) {
+		this.lauching = lauching;
+	}
+
+	public void liveStatus() {
+		System.out.println("\nСтатус робота: " + status);
+	}
+
+	public void lauching() {
+		System.out.println("\nДата спуска на воду: " + lauching);
+	}
+
+	public void murders() {
+		System.out.printf("Убил %d кайдзю", getMurder());
+	}
+
+	public Jaeger(Jaeger robot) {
+		modelName = robot.name;
+	}
+
+
+	public Jaeger(String modelName, int mark, double height, double weight, int speed, 
+        	int strength, int armor, int murder, String status, String lauching) {
+	    this.modelName = modelName;
+	    this.mark = mark;
+	    this.height = height;
+	    this.weight = weight;
+	    this.speed = speed;
+	    this.strength = strength;
+	    this.armor = armor;
+	    this.murder = murder;
+	    this.status = status;
+	    this.lauching = lauching;
+    } 
+
+    // @Override
+    // public String toString() {
+    // 	return String.format("\nМодель робота = %s \nНомер поколения: Mark-%d%n", modelName, mark);
+				// "Высота робота = %.1f метров%n Вес робота = %.0f тонн ", height, weight
+				// "Скорость робота = %d%n Сила робота = %d \nЗащита робота = %d", speed, strength, armor
+				// "Убил %d кайдзю Дата спуска на воду: Статус робота: " murder, lauching, status);
+	// }
 }
--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -1,5 +1,62 @@
 import java.util.Scanner;
 
 public class JaegerTest {
-
+    
     public static void main(String[] args) {
+        Jaeger strikerEureka = new Jaeger();
+        Jaeger crimsonTyphoon = new Jaeger(robot);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("jaeger Striker Eureka\n");
+        System.out.println("Введите имя модели робота");
+        strikerEureka.setModelName(scanner.nextLine());
+        System.out.println("Введите номер поколения");
+        strikerEureka.setMark(scanner.nextInt());
+        scanner.nextLine();
+        System.out.println("Введите высоту робота");
+        strikerEureka.setHeight(scanner.nextDouble());
+        System.out.println("Введите вес робота");
+        strikerEureka.setWeight(scanner.nextDouble());
+        System.out.println("Введите скорость робота");
+        strikerEureka.setSpeed(scanner.nextInt());
+        System.out.println("Введите силу атаки робота");
+        strikerEureka.setStrength(scanner.nextInt());
+        System.out.println("Введите мощность брони");
+        strikerEureka.setArmor(scanner.nextInt());
+        System.out.println("Введите количество убитых кайдзю");
+        strikerEureka.setMurder(scanner.nextInt());
+        scanner.nextLine();
+        System.out.println("Введите статус работоспособности робота [desabled/ enabled]");
+        strikerEureka.setStatus(scanner.nextLine());
+        System.out.println("Введите дату спуска робота на воду");
+        strikerEureka.setLauching(scanner.nextLine());
+        System.out.printf("\nМодель робота = %s \nНомер поколения: Mark-%d%n", strikerEureka.getModelName(), strikerEureka.getMark());
+        System.out.printf("Высота робота = %.1f метров%n", strikerEureka.getHeight());
+        System.out.printf("Вес робота = %.0f тонн \nСкорость робота = %d%n", strikerEureka.getWeight(), strikerEureka.getSpeed());
+        System.out.printf("Сила робота = %d \nЗащита робота = %d", strikerEureka.getStrength(), strikerEureka.getArmor());
+        strikerEureka.lauching();
+        strikerEureka.murders();
+        strikerEureka.liveStatus();
+        crimsonTyphoon.modelName = "CrimsonTyphoon";
+        crimsonTyphoon.mark = 4;
+        crimsonTyphoon.height = 76.2;
+        crimsonTyphoon.weight = 1722;
+        crimsonTyphoon.speed = 9;
+        crimsonTyphoon.strenght = 8;
+        crimsonTyphoon.armor = 6;
+        // crimsonTyphoon.murder = 7;
+        // crimsonTyphoon.status = "desabled";
+        // crimsonTyphoon.lauching = "22 августа 2018 года";
+        System.out.printf("\nМодель робота = %s \nНомер поколения: Mark-%d%n", crimsonTyphoon.modelName, crimsonTyphoon.mark);
+        System.out.printf("Высота робота = %.1f метров%n", crimsonTyphoon.height);
+        System.out.printf("Вес робота = %.0f тонн \nСкорость робота = %d%n", crimsonTyphoon.weight, crimsonTyphoon.speed);
+        System.out.printf("Сила робота = %d \nЗащита робота = %d", crimsonTyphoon.strenght, crimsonTyphoon.armor);
+    }
+
+
+    
+}    
+
+
+
+
+
