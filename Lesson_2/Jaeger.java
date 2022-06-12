ac7db81b8807ba920c423edb12fe663a0d1b6a4e
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-06-12 13:54:34 +0500

    convert tab to space HW_2.2

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -1,71 +1,71 @@
 public class Jaeger {
 
-	private String modelName = "Striker Eureka";
-	private int mark = 5;
-	private double height = 76.2;
-	private double weight = 1850;
-	private int speed = 10;
-	private int strength = 10;
-	private int armor = 9;
-	private int murder = 11;
-	private String status = "Desabled";
-	private String lauching = "November 2, 2019";
+    private String modelName = "Striker Eureka";
+    private int mark = 5;
+    private double height = 76.2;
+    private double weight = 1850;
+    private int speed = 10;
+    private int strength = 10;
+    private int armor = 9;
+    private int murder = 11;
+    private String status = "Desabled";
+    private String lauching = "November 2, 2019";
 
-	public String getModelName() {
-		return modelName;
-	}
+    public String getModelName() {
+        return modelName;
+    }
 
-	public int getMark() {
-		return mark;
-	}
+    public int getMark() {
+        return mark;
+    }
 
-	public double getHeight() {
-		return height;
-	}
+    public double getHeight() {
+        return height;
+    }
 
-	public double getWeight() {
-		return weight;
-	}
+    public double getWeight() {
+        return weight;
+    }
 
-	public int getSpeed() {
-		return speed;
-	}
+    public int getSpeed() {
+        return speed;
+    }
 
-	public int getStrength() {
-		return strength;
-	}
+    public int getStrength() {
+        return strength;
+    }
 
-	public int getArmor() {
-		return armor;
-	}
+    public int getArmor() {
+        return armor;
+    }
 
-	public int getMurder() {
-		return murder;
-	}
+    public int getMurder() {
+        return murder;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    public String getStatus() {
+        return status;
+    }
 
-	public String getLauching() {
-		return lauching;
-	}
+    public String getLauching() {
+        return lauching;
+    }
 
-	public void liveStatus() {
-		System.out.println("\nСтатус робота: " + status);
-	}
+    public void liveStatus() {
+        System.out.println("\nСтатус робота: " + status);
+    }
 
-	public void lauching() {
-		System.out.println("\nДата спуска на воду: " + lauching);
-	}
+    public void lauching() {
+        System.out.println("\nДата спуска на воду: " + lauching);
+    }
 
-	public void murders() {
-		System.out.printf("Убил %d кайдзю", getMurder());
-	}
+    public void murders() {
+        System.out.printf("Убил %d кайдзю", getMurder());
+    }
 
-	public Jaeger() {}
+    public Jaeger() {}
 
-	public Jaeger(String modelName, int mark, double height, double weight, int speed, 
+    public Jaeger(String modelName, int mark, double height, double weight, int speed, 
             int strength, int armor, int murder, String status, String lauching) {
         this.modelName = modelName;
         this.mark = mark;
@@ -79,11 +79,11 @@ public class Jaeger {
         this.lauching = lauching;
     }    
 
-	@Override
+    @Override
     public String toString() {
-    	return String.format("\nМодель робота = %s \nНомер поколения: Mark-%d \nВысота робота = %.1f метров" +
-    			"\nВес робота = %.0f тонн \nСкорость робота = %d \nСила робота = %d" +
-    			"\nЗащита робота = %d \nДата спуска на воду: %s \nУбил %d кайдзю \nСтатус робота: %s ", modelName, mark, +
-				height, weight, speed, strength, armor, lauching, murder, status);    
+        return String.format("\nМодель робота = %s \nНомер поколения: Mark-%d \nВысота робота = %.1f метров" +
+                "\nВес робота = %.0f тонн \nСкорость робота = %d \nСила робота = %d" +
+                "\nЗащита робота = %d \nДата спуска на воду: %s \nУбил %d кайдзю \nСтатус робота: %s ", modelName, mark, +
+                height, weight, speed, strength, armor, lauching, murder, status);    
     }
 }
