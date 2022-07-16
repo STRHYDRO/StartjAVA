b25d42a448636c9ef93286e3e920eab494ffde9f
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-07-16 15:55:21 +0500

    99% implement ArrayTheme

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -1,6 +1,5 @@
 package com.startjava.lesson_2_3_4.array;
 
-import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Arrays;
 
@@ -17,8 +16,8 @@ public class ArrayTheme {
             intArr[i] = intArr[intArr.length - 1 - i];
             intArr[intArr.length - 1 - i] = buffer;
         }
-
-        System.out.println();;
+        System.out.println();
+        ;
         for (int num : intArr) {
             System.out.printf("% d", num);
         }
@@ -48,15 +47,16 @@ public class ArrayTheme {
         }
         int midpoint = doubleArr.length / 2;
         System.out.println("\nИзмененный массив");
+        int counter = 0;
         for (int j = 0; j < doubleArr.length; j++) {
             if (doubleArr[j] > doubleArr[midpoint]) {
-                    doubleArr[j] = 0;
-                }
-            String format = j < midpoint ? "%.2f  " : "%.0f     ";
+                doubleArr[j] = 0;
+                counter++;
+            }
+            String format = doubleArr[j] == 0 ? "%.0f   " : "%.2f   ";
             System.out.printf(format, doubleArr[j]);
         }
-        int difference = doubleArr.length - midpoint;
-        System.out.print("\nКоличество обнуленных ячеек = " + difference);
+        System.out.print("\nКоличество обнуленных ячеек = " + counter);
 
         System.out.println("\n\nВывод элементов массива лесенкой в обратном порядке");
         char[] alphabetArr = new char[26];
@@ -64,34 +64,71 @@ public class ArrayTheme {
             alphabetArr[i] = (char) (i + 65);
         }
         for (int i = 0; i <= alphabetArr.length; i++) {
-            for (int j = alphabetArr.length - 1; j >= 0; j--) {
-                System.out.print(alphabetArr[j]);
+            int k = alphabetArr.length;
+            for (int j = 0; j < i; j++) {
+                k--;
+                System.out.print(alphabetArr[k]);
             }
             System.out.println();
-
         }
 
         System.out.println("\n\nГенерация уникальных чисел");
         Random random = new Random();
-        int[] uniqueArr = new int[30];
-        int counter = 0;
-        int min = 60;
-        int max = 101;
-        for (int i = 0; i < uniqueArr.length; i++) {
-            uniqueArr[i] = random.nextInt(41) + 60;
-        }
-        for(int i=0; i<uniqueArr.length; i++) {
-            for (int j = i + 1; j < uniqueArr.length; j++) {
-                if (uniqueArr[i] == uniqueArr[j]) {
-                    uniqueArr[i] += 1;
+            int[] uniqueArr = new int[30];
+            int meter = 0;
+            for (int i = 0; i < uniqueArr.length; i++) {
+                uniqueArr[i] = random.nextInt(40) + 60;
+            }
+            Arrays.sort(uniqueArr);
+            int[] bufferArr = new int[uniqueArr.length];
+            System.arraycopy(uniqueArr, 0, bufferArr, 0, uniqueArr.length);
+
+            for (int unique : uniqueArr) {
+                System.out.printf("%d  ", unique);
+            }
+            System.out.println();
+            for (int i = 0; i < bufferArr.length; i++) {
+                System.out.printf("%d  ", bufferArr[i]);
+            }
+            for (int i = 0; i < uniqueArr.length - 1; i++) {
+                for (int j = i + 1; j < bufferArr.length; j++) {
+                    if (uniqueArr[i] == bufferArr[j]) {
+                        uniqueArr[i] -= 1;
+                    }
                 }
             }
+        System.out.println();
+        Arrays.sort(uniqueArr);
+        for (int unique:uniqueArr) {
+            System.out.printf("%d  ", unique);
         }
+
+//        int metr = 0;
+//        do {
+//            metr++;
+//            if (uniqueArr[metr] == bufferArr[metr - 1]) {
+//                uniqueArr[metr] = 1;
+//            }
+//        } while(uniqueArr[metr] != bufferArr[metr]);
+//        System.out.println();
+//        for (int unique:uniqueArr) {
+//            System.out.printf("%d  ", unique);
+//        }
+
+        for(int i=0; i<uniqueArr.length-1; i++) {
+            for (int j=i+1; j<uniqueArr.length; j++) {
+                    if (uniqueArr[i] == uniqueArr[j]) {
+                        uniqueArr[i] += 1;
+                    }
+            }
+        }
+
+
         for (int j = 0; j < uniqueArr.length; j++) {
-            counter++;
-            if (counter == 11) {
+           meter++;
+            if (meter == 11) {
                 System.out.println();
-                counter = 1;
+                meter = 1;
             }
             Arrays.sort(uniqueArr);
             System.out.printf("%4s", uniqueArr[j]);
@@ -116,14 +153,13 @@ public class ArrayTheme {
         for (int num : indexPosition) {
             System.out.printf("% d", num);
         }
-//        String[] shiftsArrCopy = new String[indexLength];
-//        for(int i = 0; i < indexLength; i++){
-//            System.arraycopy(shiftsArr, indexPosition[i], shiftsArrCopy, i, 1);
-//        }
-//        System.out.println();
-//        for (String shiftCopy:shiftsArrCopy) {
-//            System.out.print(shiftCopy);
-//        }
+        String[] shiftsArrCopy = new String[indexLength];
+        for(int i = 0; i < indexLength; i++){
+            System.arraycopy(shiftsArr, indexPosition[i], shiftsArrCopy, i, 1);
+        }
+        System.out.println();
+        for (String shiftCopy:shiftsArrCopy) {
+            System.out.print(shiftCopy);
+        }
     }
 }
-
