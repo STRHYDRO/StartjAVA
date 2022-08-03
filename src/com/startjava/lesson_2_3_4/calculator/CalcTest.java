6828b6b6e10894107fcffa1dcc8cb7e4b4f106cc
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-08-03 17:33:24 +0500

    95% refactoring

--- src/com/startjava/lesson_2_3_4/calculator/CalcTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalcTest.java
@@ -12,9 +12,8 @@ public class CalcTest {
         do {
             if ("yes".equals(choice)) {
                 System.out.println("Введите первое число, операцию и второе число");
-                calc.setData(scanner.nextLine());
-                calc.calculate();
-                calc.format();
+                String calculate = calc.calculate(scanner.nextLine());
+                System.out.println(calculate);
             }
             System.out.println("\nХотите продолжить вычисления? [yes/no]:");
             choice = scanner.nextLine();     
--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,43 +1,21 @@
 package com.startjava.lesson_2_3_4.calculator;
 
-import java.math.*;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Calculator {
-
+    Scanner scanner = new Scanner(System.in);
     private double num1;
     private double num2;
     private char sign;
-    private double result = 0;
-    private String data;
+    private double result;
+    private String[] expressionArr = new String[3];
 
-    public double getNum1() {
-        return num1;
-    }
-
-    public double getNum2() {
-        return num2;
-    }
-
-    public char getSign() {
-        return sign;
-    }
-
-    public double getResult() {
-        return result;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    public void calculate() {
-        expression();
-        if ((getNum1() > 0 && getNum1() % 1 == 0) && (getNum2() > 0 && getNum2() % 1 == 0)) {
+    public String calculate(String expression) {
+        expressionArr =expression.split(" ");
+        num1 = Integer.parseInt(expressionArr[0]);
+        num2 = Integer.parseInt(expressionArr[2]);
+        sign = expressionArr[1].charAt(0);
+        if ((num1 > 0 && num1 % 1 == 0) && (num2 > 0 && num2 % 1 == 0)) {
             switch (sign) {
                 case '+':
                     result = num1 + num2;
@@ -61,23 +39,10 @@ public class Calculator {
         } else {
             System.out.println("\nЧисла отрицательные или не целые, для расчета введите целые положительные числа");
         }
-    }
-
-    private void expression() {
-        String[] parentArr = new String[2];
-        String[] expressionArr = new String[3];
-        parentArr = data.split("[\\s\\+\\-\\*\\/\\^\\%]+");
-        for (int i = 0; i < parentArr.length; i++) {
-            expressionArr[i] = parentArr[i];
-        }
-        expressionArr[2] = data.replaceAll("\\d\\s*", "");
-        num1 = Integer.parseInt(expressionArr[0]);
-        num2 = Integer.parseInt(expressionArr[1]);
-        sign = expressionArr[2].charAt(0);
-    }
-
-    private void format() {
-        String format = getResult() % 1 == 0 ? "%.0f %c %.0f = %.0f" : "%.0f %c %.0f = %.2f";
-        System.out.printf(format, getNum1(), getSign(), getNum2(), getResult());
+//        String format(result % 1 == 0 ? "%.0f %c %.0f = %.0f" : "%.0f %c %.0f = %.2f");
+//        String conclusion = "(num1, sign, num2, result)";
+        String str = Double.toString(result);
+        return str;
     }
 }
+
