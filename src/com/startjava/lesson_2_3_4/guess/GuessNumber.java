16d59d3cec18c638fa928d240679dce9662ed941
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-08-08 16:20:51 +0500

    10% implement

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -9,39 +9,41 @@ public class GuessNumber {
     private Player player1;
     private Player player2;
     private Player currName;
-
+    int counter = 0;
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
     }
 
     public void play() {
+
         randomNumber = (int) (Math.random() * (100 + 1));
         do {
+            counter++;
             changePlayer();
             System.out.println("У каждого игрока по 10 попыток");
             System.out.printf("Игрок %s введите целое число от 0 до 100\n", currName.getName());
-            currName.setNum(scan.nextInt());
+            currName.setEnteredNums(scan.nextInt());
             scan.nextLine();
-            if (currName.getNum() > 0 && currName.getNum() % 1 == 0) {
-                if (currName.getNum() == randomNumber) {
-                    System.out.printf("Игрок : %s угадал число %d с %d попытки", currName.getName(), randomNumber, counter);
-                    break;
-                } else if (currName.getNum() < randomNumber) {
-                    System.out.printf("число %s меньше того, что загадал компьютер\n", currName.getNum());
-                } else if (currName.getNum() > randomNumber) {
-                    System.out.printf("число %s больше того, что загадал компьютер\n", currName.getNum());
-                }
-            } else {
-                System.out.println("Вы ввели не подходящее число, вводите только целые положительные числа");
+            for(int arr:currName.getEnteredNums()) {
+                System.out.println(arr);
             }
+//            if (currName.getEnteredNums() > 0 && currName.getEnteredNums() % 1 == 0) {
+//                if (currName.getNum() == randomNumber) {
+//                    System.out.printf("Игрок : %s угадал число %d с %d попытки", currName.getName(), randomNumber, counter);
+//                    break;
+//                } else if (currName.getNum() < randomNumber) {
+//                    System.out.printf("число %s меньше того, что загадал компьютер\n", currName.getNum());
+//                } else if (currName.getNum() > randomNumber) {
+//                    System.out.printf("число %s больше того, что загадал компьютер\n", currName.getNum());
+//                }
+//            } else {
+//                System.out.println("Вы ввели не подходящее число, вводите только целые положительные числа");
+//            }
         } while(true);
     }
 
-    private void inpit() {
 
-
-    }
 
     private void changePlayer() {
         currName = player1 == currName ? player2 : player1;
--- src/com/startjava/lesson_2_3_4/guess/Player.java
+++ src/com/startjava/lesson_2_3_4/guess/Player.java
@@ -1,9 +1,9 @@
 package com.startjava.lesson_2_3_4.guess;
 
 public class Player {
-
     private String name;
-    private int[] numArr = new int[10];
+    private int[] enteredNums = new int[10];
+
 
     public Player(String name) {
         this.name = name;
@@ -13,13 +13,15 @@ public class Player {
         return name;
     }
 
-    public int[] getNumArr() {
-        return numArr;
+    public int[] getEnteredNums() {
+        return enteredNums;
     }
 
-    public void setNumArr(int[] numArr) {
-        this.numArr = numArr;
+    public void setEnteredNums(int enteredNum) {
+        int lenght = enteredNums.length;
+        for (int i = 0; i < 1++; i++) {
+            enteredNums[i] = enteredNum;
+        }
     }
 }
-
 
