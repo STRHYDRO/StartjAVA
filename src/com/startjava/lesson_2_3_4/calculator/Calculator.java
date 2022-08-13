8458dce77131323def9c8e7231ec9535b931ed0f
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-08-13 18:33:47 +0500

    no implement try- catch

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,26 +2,22 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public double calculate(String expression) {
-        String[] partsExpression = new String[3];
-        partsExpression = expression.split("[\\s]");
-        double num1 = Integer.parseInt(partsExpression[0]);
-        double num2 = Integer.parseInt(partsExpression[2]);
-        char sign = partsExpression[1].charAt(0);
+    protected static double calculate(String expression) {
+        String[] partsExpression = expression.split("[\\s]");
+        try {
+            int num1 = Integer.parseInt(partsExpression[0]);
+            int num2 = Integer.parseInt(partsExpression[2]);
+            char sign = partsExpression[1].charAt(0);
+        }
+
         if (check(num1, num2)) {
             switch (sign) {
-                case '+':
-                    return num1 + num2;
-                case '-':
-                    return num1 - num2;
-                case '*':
-                    return num1 * num2;
-                case '/':
-                    return num1 / num2;
-                case '^':
-                    return Math.pow(num1, num2);
-                case '%':
-                    return Math.IEEEremainder(num1, num2);
+                case '+' -> { return Math.addExact(num1, num2); }
+                case '-' -> { return Math.subtractExact(num1, num2); }
+                case '*' -> { return Math.multiplyExact(num1, num2); }
+                case '/' -> { return (double) num1 / num2; }
+                case '^' -> { return Math.pow(num1, num2); }
+                case '%' -> { return Math.IEEEremainder(num1, num2); }
             }
         } else {
             System.out.println("\nЧисла отрицательные или не целые, для расчета введите целые положительные числа");
@@ -29,8 +25,9 @@ public class Calculator {
         return 0;
     }
 
-    private boolean check(double num1, double num2) {
+    private static boolean check(int num1, int num2) {
         return (num1 > 0 && num1 % 1 == 0) && (num2 > 0 && num2 % 1 == 0);
     }
+
 }
 
