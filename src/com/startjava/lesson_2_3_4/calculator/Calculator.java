3d54eb2b9081542149c3fbea78ed572ff58e7bfb
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-08-14 18:49:41 +0500

    no implement try- catch

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,34 +2,34 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public double calculate(String expression) {
-        inpit();
-        if (check(num1, num2)) {
-            switch (sign) {
-                case '+' -> { return Math.addExact(num1, num2); }
-                case '-' -> { return Math.subtractExact(num1, num2); }
-                case '*' -> { return Math.multiplyExact(num1, num2); }
-                case '/' -> { return (double) num1 / num2; }
-                case '^' -> { return Math.pow(num1, num2); }
-                case '%' -> { return Math.IEEEremainder(num1, num2); }
-            }
-        } else {
-            System.out.println("\nЧисла отрицательные или не целые, для расчета введите целые положительные числа");
-        }
-        return 0;
-    }
-
-    private static boolean check(int num1, int num2) {
-        return (num1 > 0 && num1 % 1 == 0) && (num2 > 0 && num2 % 1 == 0);
-    }
-
-    public double inpit(String expression) {
+    public static double calculate(String expression) {
         String[] partsExpression = expression.split("[\\s]");
         int num1 = Integer.parseInt(partsExpression[0]);
         int num2 = Integer.parseInt(partsExpression[2]);
         char sign = partsExpression[1].charAt(0);
-        
+        boolean check = false;
 
+        if (check == true) {
+            return switch (sign) {
+                case '+' -> Math.addExact(num1, num2);
+                case '-' -> Math.subtractExact(num1, num2);
+                case '*' -> Math.multiplyExact(num1, num2);
+                case '/' -> (double) num1 / num2;
+                case '^' -> Math.pow(num1, num2);
+                case '%' -> Math.IEEEremainder(num1, num2);
+                default -> 0;
+            };
+        }
+        return 0;
+    }
+
+    private static boolean check(int num1, int num2, boolean check) {
+        if ((num1 > 0 && num1 % 1 == 0) && (num2 > 0 && num2 % 1 == 0)) {
+            return check = true;
+        } else {
+            System.out.println("\nЧисла отрицательные или не целые, для расчета введите целые положительные числа");
+            return check = false;
+        }
     }
 }
 
--- src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
@@ -1,24 +1,24 @@
-package com.startjava.lesson_2_3_4.guess;
-
-import java.util.Scanner;
-
-public class GuessNumberTest {
-
-    public static void main(String[] args) {
-        System.out.println("Игра: угадай число_3\n");
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите имя первого игрока");
-        Player player1 = new Player(scan.nextLine());
-        System.out.println("Введите имя второго игрока");
-        Player player2 = new Player(scan.nextLine());
-        GuessNumber game = new GuessNumber(player1, player2);
-        String choice = "yes";
-        do {
-            if ("yes".equals(choice)) {
-                game.play();
-            }
-            System.out.println("\nХотите продолжить игру? [yes/no]:");
-            choice = scan.nextLine();
-        } while(!"no".equals(choice));
-    }
-}
+//package com.startjava.lesson_2_3_4.guess;
+//
+//import java.util.Scanner;
+//
+//public class GuessNumberTest {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Игра: угадай число_3\n");
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Введите имя первого игрока");
+//        Player player1 = new Player(scan.nextLine());
+//        System.out.println("Введите имя второго игрока");
+//        Player player2 = new Player(scan.nextLine());
+//        GuessNumber game = new GuessNumber(player1, player2);
+//        String choice = "yes";
+//        do {
+//            if ("yes".equals(choice)) {
+//                game.play();
+//            }
+//            System.out.println("\nХотите продолжить игру? [yes/no]:");
+//            choice = scan.nextLine();
+//        } while(!"no".equals(choice));
+//    }
+//}
