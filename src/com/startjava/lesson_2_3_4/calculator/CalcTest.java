b9172d23dd720515139e8c3c7999f445c07400ef
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-08-15 14:49:10 +0500

    no implement try- catch

--- src/com/startjava/lesson_2_3_4/calculator/CalcTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalcTest.java
@@ -5,20 +5,50 @@ import java.util.Scanner;
 public class CalcTest {
 
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Calculator test\n");
         String choice = "yes";
         do {
             if ("yes".equals(choice)) {
                 System.out.println("Введите первое число, операцию и второе число");
-                Double result = calc.calculate(scanner.nextLine());
+                Double result = calculate(scanner.nextLine());
                 System.out.print(result);
             }
             System.out.println("\nХотите продолжить вычисления? [yes/no]:");
             choice = scanner.nextLine();     
         } while (!"no".equals(choice));
-    } 
+    }
+
+    static double calculate(String expression) {
+        String[] partsExpression = expression.split("[\\s]");
+        int num1 = Integer.parseInt(partsExpression[0]);
+        int num2 = Integer.parseInt(partsExpression[2]);
+        char sign = partsExpression[1].charAt(0);
+        boolean check = false;
+        double result = 0;
+        
+        check(num1, num2, check);
+        if (check == true) {
+            switch (sign) {
+                case '+' -> result = Math.addExact(num1, num2);
+                case '-' -> result = Math.subtractExact(num1, num2);
+                case '*' -> result = Math.multiplyExact(num1, num2);
+                case '/' -> result = (double) num1 / num2;
+                case '^' -> result = Math.pow(num1, num2);
+                case '%' -> result = Math.IEEEremainder(num1, num2);
+            };
+        } return result;
+    }
+
+    private static boolean check(int num1, int num2, boolean check) {
+        if ((num1 > 0 && num1 % 1 == 0) && (num2 > 0 && num2 % 1 == 0)) {
+            return true;
+        } else {
+            System.out.println("\nЧисла отрицательные или не целые, для расчета введите целые положительные числа");
+            return false;
+        }
+    }
 }
+
 
 
--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -1,64 +1,66 @@
-//package com.startjava.lesson_2_3_4.guess;
-//
-//import java.util.Arrays;
-//import java.util.Scanner;
-//
-//public class GuessNumber {
-//
-//    Scanner scan = new Scanner(System.in);
-//    private int randomNumber;
-//    private Player player1;
-//    private Player player2;
-//    private Player currentPlayer;
-//
-//    public GuessNumber(Player player1, Player player2) {
-//        this.player1 = currentPlayer;
-//        this.player2 = player2;
-//        this.currentPlayer = player1;
-//    }
-//
-//    public void play() {
-//        randomNumber = 1 + (int) (Math.random() * 100);
-//        System.out.println("У каждого игрока по 10 попыток");
-//        boolean check = false;
-//        System.out.printf("Игрок %s введите целое число от 1 до 100\n", currentPlayer.getName());
-//        int num = currentPlayer.addNum(scan.nextInt());
-//        scan.nextLine();
-//        check(num, check);
-//            while (check)
-//            if (num == randomNumber) {
-//                System.out.printf("Игрок : %s угадал число %d с %d попытки\n",currentPlayer.getName(), randomNumber, currentPlayer.getAttempt());
-//                inpitNums();
-//                break;
-//            } else if (num < randomNumber) {
-//                System.out.printf("число %s меньше того, что загадал компьютер\n", num);
-//            } else {
-//                System.out.printf("число %s больше того, что загадал компьютер\n", num);
-//            }
-//            changePlayer();
-//            currentPlayer.reset();
-//        } while(true);
-////    }
-//
-//    private void changePlayer() {
-////        currentPlayer == player1 ? player2 : player1;;
-//    }
-//
-//    private boolean check(int num, boolean check) {
-//        if (num > 0)  {
-//            check = true;
-//        } else {
-//            check = false;
-//            System.out.println("Вы ввели не подходящее число, вводите только целые положительные числа");
-//        }
-//        return check;
-//    }
-//
-//    private void inpitNums(Player currentPlayer) {
-//        System.out.print("Игрок " + currentPlayer + " ввел числа: ");
-//        for (int nums : currentPlayer.getEnteredNums()) {
-//            System.out.print("  " + nums);
-//        }
-//    }
-//}
+package com.startjava.lesson_2_3_4.guess;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GuessNumber {
+
+    Scanner scan = new Scanner(System.in);
+    private int randomNumber;
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+
+    public GuessNumber(Player player1, Player player2) {
+        this.player1 = currentPlayer;
+        this.player2 = player2;
+        this.currentPlayer = player1;
+    }
+
+    public void play() {
+        randomNumber = 1 + (int) (Math.random() * 100);
+        boolean check = false;
+
+        System.out.println("У каждого игрока по 10 попыток");
+        System.out.printf("Игрок %s введите целое число от 1 до 100\n", currentPlayer.getName());
+        int num = currentPlayer.addNum(scan.nextInt());
+        scan.nextLine();
+
+        check(num, check);
+        while (check == true) {
+            if (num == randomNumber) {
+                System.out.printf("Игрок : %s угадал число %d с %d попытки\n", currentPlayer.getName(), randomNumber, currentPlayer.getAttempt());
+                inpitNums(currentPlayer);
+                break;
+            } else if (num < randomNumber) {
+                System.out.printf("число %s меньше того, что загадал компьютер\n", num);
+            } else {
+                System.out.printf("число %s больше того, что загадал компьютер\n", num);
+            }
+            changePlayer();
+            currentPlayer.reset();
+        }
+    }
+
+    private void changePlayer() {
+        currentPlayer = currentPlayer == player1 ? player2 : player1;
+    }
+
+    private boolean check(int num, boolean check) {
+        if (num > 0)  {
+            check = true;
+        } else {
+            check = false;
+            System.out.println("Вы ввели не подходящее число, вводите только целые положительные числа");
+        }
+        return check;
+    }
+
+    private void inpitNums(Player currentPlayer) {
+        System.out.print("Игрок " + currentPlayer + " ввел числа: ");
+        for (int nums : currentPlayer.getEnteredNums()) {
+            System.out.print("  " + nums);
+        }
+    }
+}
 
