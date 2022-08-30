f451fef461273b36935f14ccdee63c119dae4945
STRHYDRO <103580094+STRHYDRO@users.noreply.github.com>
2022-08-30 16:52:12 +0500

    + implement, no normal work method str 50.

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -19,33 +19,37 @@ public class GuessNumber {
 
     public void play() {
         int randomNumber = 1 + (int) (Math.random() * 100);
+        boolean playerWin = false;
 
         System.out.println("У каждого игрока по 10 попыток");
-        while (currentPlayer.getAttempt() != maxAttempts) {
+        while (currentPlayer.getAttempt() != maxAttempts && !playerWin) {
             System.out.printf("Игрок %s введите целое число от 1 до 100\n", currentPlayer.getName());
             int num = currentPlayer.addNum(scan.nextInt());
 
-            if(checkPositiveNum(num)) {
+            if(checkIsPositive(num)) {
                 if (num == randomNumber) {
-                    System.out.printf("Победил игрок %s, угадав число %d с %d попытки\n", currentPlayer.getName(),
-                            randomNumber, currentPlayer.getAttempt());
-                    break;
+                    System.out.printf("Победил игрок %s, угадав число %d с %d попытки\n",
+                            currentPlayer.getName(), randomNumber, currentPlayer.getAttempt());
+                    playerWin = true;
+                } else {
+                    String moreLess = num < randomNumber ? "меньше" : "больше";
+                    System.out.printf("Число %d %s того что загадал компьютер\n", num, moreLess);
+                    changePlayer();
                 }
-                String moreLess = num < randomNumber ? "меньше" : "больше";
-                System.out.printf("Число %d %s того что загадал компьютер\n", num, moreLess);
-                changePlayer();
             }
         }
         showEnteredNums(player1);
         showEnteredNums(player2);
+        currentPlayer.reset();
     }
 
     private void changePlayer() {
         currentPlayer = currentPlayer == player1 ? player2 : player1;
     }
 
-    private boolean checkPositiveNum(int num) {
-        if (currentPlayer.getEnteredNum() <= 0) {
+    private boolean checkIsPositive(int num) {
+        changePlayer();
+        if (currentPlayer.getEnteredNum() < 1) {
             System.out.println("Вы ввели не подходящее число, вводите только целые положительные числа");
             return false;
         }
@@ -54,8 +58,8 @@ public class GuessNumber {
 
     private void showEnteredNums(Player currentPlayer) {
         System.out.print("Игрок " + currentPlayer.getName() + " назвал числа:" );
-        for (int nums : currentPlayer.getEnteredNums()) {
-            System.out.print("  " + nums);
+        for (int num : currentPlayer.getEnteredNums()) {
+            System.out.print("  " + num);
         }
         System.out.println();
     }
